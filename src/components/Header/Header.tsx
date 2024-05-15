c1152f8072c23cb010791392b407605db6a67570
joankenobi <joan1233blanco@gmail.com>
2024-05-15 19:13:16 -0400

    feat: added header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { AppBar, Box, Toolbar, InputBase, Typography } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+
+function Header(): JSX.Element {
+
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h6">
+                    <Search />
+                    Search
+                </Typography>
+                <Box display={"flex"}>
+                    <Typography>
+                        Explore new places
+                    </Typography>
+                    <span className='///////aqui va el autocomplete de google////////'>
+                        <div className={"classes.search"}>
+                            <div className={"classes.searchIcon"}>
+                                <Search />
+                            </div>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: "classes.inputRoot",
+                                    input: "classes.inputInput",
+                                }}
+                                inputProps={{ "aria-label": "search" }} />
+                        </div>
+                    </span>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
