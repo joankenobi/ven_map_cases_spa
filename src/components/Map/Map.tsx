839c76fd79cec0bde129dfbf01b2fffa8b0abed7
joankenobi <53411488+joankenobi@users.noreply.github.com>
2024-05-17 01:32:46 +0000

    probando mapa

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,20 @@
 import { Typography } from "@material-ui/core"
+import * as React from 'react';
+import Map from 'react-map-gl/maplibre';
 
 Typography
 
 function Map(){
     return(
-        <Typography display="block">
-            Map
-        </Typography>
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="https://api.maptiler.com/maps/streets/style.json?key=get_your_own_key"
+    />
     )
 }
 
