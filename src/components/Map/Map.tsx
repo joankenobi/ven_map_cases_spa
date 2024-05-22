427b2fdc64833a385039243350090b6da98c79c9
joankenobi <joan1233blanco@gmail.com>
2024-05-22 07:33:05 -0400

    update Mapcomp

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,19 +1,37 @@
 // import { Typography } from "@material-ui/core"
-// import * as React from 'react';
-import Map from 'react-map-gl/maplibre';
+import React, {useState, useEffect, useRef} from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function Mapcomp(){
-    return(
-    <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="https://api.maptiler.com/maps/streets/style.json?key=get_your_own_key"
-    />
-    )
+  const mapContainer = useRef(null);
+  const [viewState, setViewState] = useState({
+    center:[-100.43,35],
+    zoom: 3,
+    pitch: 50,
+  });
+
+  React.useEffect(() => {
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_key',
+      ...viewState
+    });
+    
+    return () => {
+      map.remove();
+      
+    };
+  },[]);
+
+  return (
+    <>
+    <div ref={mapContainer} 
+    style={{width: '100%', height: '100vh', position: 'absolute'}} >
+    </div>
+    </>
+  );
 }
+
 
 export default Mapcomp
